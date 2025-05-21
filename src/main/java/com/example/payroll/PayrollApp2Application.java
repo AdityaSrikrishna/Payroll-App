a4d46f580853b4767ee03293567c6ffa3cb69351
Sri Krishna Aditya Garimella <gskaditya007@gmail.com>
2025-05-21 14:42:09 +0530

    Employee Payroll Main class

---
+++ src/main/java/com/example/payroll/PayrollApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollApp2Application.class, args);
+	}
+
+}
